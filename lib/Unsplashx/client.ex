22043e65a87cecc4d55f156cc6f0296766b0a508
lijiaxi <psi.xie@gmail.com>
2023-02-05 21:00:01 +0800

    Add metadata

--- lib/Unsplashx/client.ex
+++ lib/Unsplashx/client.ex
@@ -10,9 +10,27 @@ defmodule Tesla.Middleware.UnsplashxSimpleResponse do
   def build_response({:error, reasonn}), do: {:error, reasonn}
 
   def build_response({:ok, env}) do
-    body = env.body
+    data = env.body
+    IO.inspect(env.headers)
 
-    {:ok, body}
+    metadata =
+      env.headers
+      |> take_headers(["x-total", "x-per-page"])
+      |> Enum.map(fn {k, v} ->
+        k = String.replace_prefix(k, "x-", "")
+        {k, v}
+      end)
+      |> Enum.into(%{})
+    {:ok, %{data: data, metadata: metadata}}
+  end
+
+  def take_headers(headers, keys) do
+    Enum.reduce_while(keys, [], fn key, acc ->
+      # [v | _] = for {^key, value} <- headers, do: value
+      value = Enum.find(headers, fn {k, _} -> k == key end)
+      IO.inspect(value)
+      if value != nil, do: {:cont, [value | acc]}, else: {:halt, acc}
+    end)
   end
 end
 
@@ -23,12 +41,11 @@ defmodule Unsplashx.Client do
 
   @decode_content_types ["application/json", "text/javascript", "text/plain"]
 
-  adapter Tesla.Adapter.Hackney
+  adapter(Tesla.Adapter.Hackney)
 
-  plug Tesla.Middleware.UnsplashxSimpleResponse
-  plug Tesla.Middleware.JSON, decode_content_types: @decode_content_types
+  plug(Tesla.Middleware.UnsplashxSimpleResponse)
+  plug(Tesla.Middleware.JSON, decode_content_types: @decode_content_types)
 
-  plug Tesla.Middleware.Headers, [{"authorization", "Client-ID #{get_client_id()}"}]
-  plug Tesla.Middleware.BaseUrl, get_base_url()
-
+  plug(Tesla.Middleware.Headers, [{"authorization", "Client-ID #{get_client_id()}"}])
+  plug(Tesla.Middleware.BaseUrl, get_base_url())
 end
