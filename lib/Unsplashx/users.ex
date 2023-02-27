55db1f602fdd15d342c18e197a87ec5d634f0999
lijiaxi <psi.xie@gmail.com>
2023-02-27 22:23:24 +0800

    Update

--- lib/Unsplashx/users.ex
+++ lib/Unsplashx/users.ex
@@ -1,6 +1,11 @@
 defmodule Unsplashx.Users do
   alias Unsplashx.Client
 
+  def profile(username) do
+    {:ok, res} = Client.get("/users/#{username}")
+    res
+  end
+
   def likes(username, query \\ []) do
     {:ok, res} = Client.get("/users/#{username}/likes", query: query)
     res
